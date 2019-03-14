39babc004db8ca0730308795c8e602cc1bad5e50
sachinthakare-cohesity <48554993+sachinthakare-cohesity@users.noreply.github.com>
2019-03-14 17:49:34 +0530

    Update add_vm_to_protection_and_run.py

--- cohesity_management_sdk/samples/add_vm_to_protection_and_run/add_vm_to_protection_and_run.py
+++ cohesity_management_sdk/samples/add_vm_to_protection_and_run/add_vm_to_protection_and_run.py
@@ -1,19 +1,19 @@
 # Copyright 2019 Cohesity Inc.
 #
+# This script is compatible with both Python2 and Python3
+#
 # Python example to add a VM to a protection Job.
 # Note: This script assumes you have a vCenter as a protection source in your
 # Cohesity Cluster.
 # Usage: python add_vm_to_protection_and_run.py \
 # --job_name=<name_of_protection_job> --vm_list=<list_of_vms> --run_now=True
 
+import os
 import argparse
 
 from cohesity_management_sdk.cohesity_client import CohesityClient
 from cohesity_management_sdk.samples.on_demand_job_run.on_demand_job_run import ProtectionJobs
 
-CLUSTER_USERNAME = 'cluster_username'
-CLUSTER_PASSWORD = 'cluster_password'
-CLUSTER_VIP = 'prod-cluster.cohesity.com'
 
 class AddVMProtectionJob(object):
 
@@ -50,7 +50,7 @@ class AddVMProtectionJob(object):
         """
         str_vm_list = ','.join(vm_list)
         # Note: This API works only with vCenter VMs.
-        print str_vm_list
+        print(str_vm_list)
         resp_vm_list = self.cohesity_client.protection_sources\
             .list_virtual_machines(names=str_vm_list)
 
@@ -72,13 +72,104 @@ class AddVMProtectionJob(object):
                 return True, job
         return False, None
 
+def get_mgnt_token():
+    # Get the Environment variables from App Container.
+    app_auth_token = os.getenv('APP_AUTHENTICATION_TOKEN')
+    app_endpoint_ip = os.getenv('APPS_API_ENDPOINT_IP')
+    app_endpoint_port = os.getenv('APPS_API_ENDPOINT_PORT')
 
-def main(args):
-    cohesity_client = CohesityClient(cluster_vip=CLUSTER_VIP,
-                                     username=CLUSTER_USERNAME,
-                                     password=CLUSTER_PASSWORD)
 
-    cohesity_client.config.cluster_vip = CLUSTER_VIP
+    # Initialize the client.
+    app_cli = AppClient(app_auth_token, app_endpoint_ip, app_endpoint_port)
+    app_cli.config.disable_logging()
+
+    # Get the settings information.
+    settings = app_cli.settings
+    print(settings.get_app_settings())
+
+    # Get the management access token.
+    token = app_cli.token_management
+    mgmt_auth_token = token.create_management_access_token()
+    return mgmt_auth_token
+
+def get_cmdl_args():
+    parser = argparse.ArgumentParser(description="Arguments needed to run "
+                                                 "python scripts eg. "
+                                                 "cluster_vip,"
+                                                 "UserName & Password")
+    parser.add_argument("-i", "--cluster_vip", help="Cluster VIP to login")
+    parser.add_argument("-u", "--user", help="Username to login")
+    parser.add_argument("-p", "--password", help="password to login")
+    args = parser.parse_args()
+    return args
+
+def get_mgnt_token():
+    """
+    To get the management access token from athena to authenticate
+    :return: mgmt_auth_token
+    """
+    # Get the Environment variables from App Container.
+    app_auth_token = os.getenv('APP_AUTHENTICATION_TOKEN')
+    app_endpoint_ip = os.getenv('APPS_API_ENDPOINT_IP')
+    app_endpoint_port = os.getenv('APPS_API_ENDPOINT_PORT')
+
+
+    # Initialize the client.
+    app_cli = AppClient(app_auth_token, app_endpoint_ip, app_endpoint_port)
+    app_cli.config.disable_logging()
+
+    # Get the settings information.
+    settings = app_cli.settings
+    print(settings.get_app_settings())
+
+    # Get the management access token.
+    token = app_cli.token_management
+    mgmt_auth_token = token.create_management_access_token()
+    return mgmt_auth_token
+
+
+def get_cmdl_args():
+    """"
+    To accept all commandline arguments eg userId and password
+    """
+    parser = argparse.ArgumentParser(description="Arguments needed to run "
+                                                 "python scripts eg. "
+                                                 "cluster_vip,"
+                                                 "UserName & Password")
+    parser.add_argument("-i", "--cluster_vip", help="Cluster VIP to login")
+    parser.add_argument("-u", "--user", help="Username to login")
+    parser.add_argument("-p", "--password", help="password to login")
+    parser.add_argument("--job_name", help="Name of the Protection Job.",
+                        required=True)
+    parser.add_argument("--vm_list", help="Name of the VM to be added to "
+                                          "Protection Job.", required=True,
+                        type=str)
+    parser.add_argument("--run_now", help="Specify True to run job now.",
+                        default=False, type=bool)
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    # Login to the cluster
+    args = get_cmdl_args()
+    if args.cluster_vip is not None and args.user is not None and \
+    args.password is not None:
+        cohesity_client = CohesityClient(cluster_vip=args.cluster_vip,
+                                         username=args.user,
+                                         password=args.password)
+    elif args.cluster_vip is not None or args.user is not None or \
+    args.password is not None:
+        print("Please provide all inputs ie. cluster_vip, user & password")
+        exit()
+    else:
+        host_ip = os.getenv('HOST_IP')
+        mgmt_auth_token = get_mgnt_token()
+        cohesity_client = CohesityClient(cluster_vip=host_ip,
+                                         auth_token=mgmt_auth_token)
+
+    cohesity_client.config.cluster_vip = args.cluster_vip
     vm_protect = AddVMProtectionJob(cohesity_client)
 
     # add those vms to a protection job
@@ -91,12 +182,8 @@ def main(args):
         protect_object.run_job(args.job_name)
         print("Protection Job run successfully")
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Arguments needed to run this python process.')
-    parser.add_argument("--job_name", help="Name of the Protection Job.", required=True)
-    parser.add_argument("--vm_list", help="Name of the VM to be added to "
-                                          "Protection Job.", required=True,
-                        type=str)
-    parser.add_argument("--run_now", help="Specify True to run job now.", default=False, type=bool)
-    args = parser.parse_args()
-    main(args)
+
+    main()
+
